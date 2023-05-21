87832072aa36d48d88331b368996c76bcd203c35
Gaurav Chandrakar | GC <34914837+gauravchandrakar25@users.noreply.github.com>
2023-05-21 14:18:28 +0530

    Initial-commit

---
+++ client/index.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+const prompt = require("prompt-sync")();
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+
+  // Create the merkle tree and get the Root.
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot(); //console.log the root and hardcode it to server side.
+
+  // Ask user input to get the name which will be searched and be sure it is not null.
+  const name = prompt("Which name do you want to check?");
+  if(name == null){
+    alert("You have to type a name");
+  }
+
+  // Find the index of the name and get proof from merkle tree.
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    name,
+    proof
+  });
+
+  console.log({ gift });
+}
+
+main();
---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const verifyProof = require('../utils/verifyProof');
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+const MERKLE_ROOT = '088d3c1ec641306d31cd1e27a5f93ff2d3d9dedd612dadbe731c0d24cdbc7a65';
+
+app.post('/gift', (req, res) => {
+  // Grab the parameters from the client side
+  const { name, proof } = req.body;
+
+  // Prove that a name is in the list 
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if(isInTheList) {
+    res.send("You got a toy robot!");
+  }
+  else {
+    res.send("You are not on the list :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
